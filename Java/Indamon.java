93863fe6df5fc0f5e2bb9b85be69039ea564c080
root <root@DESKTOP-UV0CABD>
2023-10-28 18:55:59 +0200

    setters and getters

--- Java/Indamon.java
+++
@@ -1,9 +0,0 @@
-class Indamon {
-    // Feilds
-    private String name;
-    private int hp;
-    private int attack;
-    private int defence;
-    private boolean fainted;
-
-}
---
+++ Java/Indamon/Indamon.java
@@ -0,0 +1,43 @@
+package Indamon;
+
+
+public class Indamon {
+      // Feilds
+    private String name;
+    private int hp;
+    private int attack;
+    private int defense;
+    private boolean fainted;
+
+    public String getName() {
+        return this.name;} 
+    public void setName(String newName) {
+        name = newName;}
+
+    public int getHp() {
+        return this.hp;}
+    public void setHp(int newHp) {
+        hp = newHp;}
+
+    public int getAttack() {
+        return attack;}
+    public void setAttack(int newAttack) {
+        attack = newAttack;}
+
+    public int getDefense() {
+        return defense;}
+    public void setDefense(int newDefense) {
+        defense = newDefense;}
+    
+    public boolean getFainted() {
+        return fainted;}
+    public void setFainted(boolean newFainted) {
+        fainted = newFainted;
+    }
+}
+
+  
+
+
+    
+    
