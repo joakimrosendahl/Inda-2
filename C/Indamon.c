eb643c1a66777aa13dfc5b194cce9704add1cf90
root <root@DESKTOP-UV0CABD>
2023-11-01 14:16:53 +0100

    uppdate

--- C/Indamon.c
+++ C/Indamon.c
@@ -1,7 +1,18 @@
-typedef structure {
-    char name;
+#include <stdbool.h>
+#include <stdio.h> 
+#include <string.h>
+
+typedef struct {
+    char name[10];
     int hp;
     int attack;
     int defense;
     bool fainted;
 } Indamon;
+
+char* getName(Indamon* instance) {
+    return instance -> name;
+}
+void setName(Indamon* instance, char* newName) {
+    strcpy(instance -> name, newName);
+}
---
+++ C/Main.c
@@ -0,0 +1,9 @@
+#include <Indamon.c>
+
+
+Indamon pika;
+
+int main() {
+    
+    return 0;
+}
